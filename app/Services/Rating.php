58d81e95b87150c436f169d0cff3e1712117c5c9
Tamas Mohos <zerosuxx@gmail.com>
2020-01-13 13:56:03 +0100

    EMEVW-9: adds Rating service
    Co-authored-by: David Szigecsan <david.szigecsan@emarsys.com>

---
+++ app/Services/Rating.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Exceptions\RatingNotFound;
+use Illuminate\Redis\RedisManager;
+
+class Rating
+{
+    private $redisManager;
+
+    public function __construct(RedisManager $redisManager)
+    {
+        $this->redisManager = $redisManager;
+    }
+
+    public function upVote(int $articleId, int $customerId, int $adminId): void
+    {
+        $this->redisManager->set("${articleId}.${customerId}.${adminId}", 'up');
+    }
+
+    public function downVote(int $articleId, int $customerId, int $adminId): void
+    {
+        $this->redisManager->set("${articleId}.${customerId}.${adminId}", 'down');
+    }
+
+    public function deleteVote(int $articleId, int $customerId, int $adminId): void
+    {
+        $this->redisManager->delete("${articleId}.${customerId}.${adminId}");
+    }
+
+    public function getVote(int $articleId, int $customerId, int $adminId): string
+    {
+        $result = $this->redisManager->get("${articleId}.${customerId}.${adminId}");
+
+        if ($result === null) {
+            throw new RatingNotFound(sprintf(
+                'Rating not found for given key: %d.%d.%d',
+                $articleId,
+                $customerId,
+                $adminId
+            ));
+        }
+
+        return $result;
+    }
+}
